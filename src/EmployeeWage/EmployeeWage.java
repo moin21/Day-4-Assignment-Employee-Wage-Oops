f5e7c9d732db0b58464e4d21dbe643bbb2c0a2d3
moin21 <mamoinuddin@gmail.com>
2022-02-17 20:09:18 +0530

    Added: Logic for daily wage calculation based on attendance

--- src/EmployeeWage/EmployeeWage.java
+++ src/EmployeeWage/EmployeeWage.java
@@ -3,24 +3,33 @@ package EmployeeWage;
 import java.util.Random;
 
 class Employee {
-    static final int IS_FULL_TIME = 1;
+	static final int IS_FULL_TIME = 1;
+	static final int EMP_WAGE_PER_DAY = 20;
 
-    public static void empCheck() {
-	Random random = new Random();
-	int empCheck = random.nextInt(2);
-	if (empCheck == IS_FULL_TIME) {
-	    System.out.println("PRESENT");
-	} else {
-	    System.out.println("ABSENT");
+	public static int empCheck() {
+		Random random = new Random();
+		int empCheck = random.nextInt(2);
+		return empCheck;
 	}
-    }
+
+	public static void empSalary(int empCheck) {
+		int empWrkHrs = 0;
+		if (empCheck == IS_FULL_TIME) {
+			empWrkHrs = 8;
+		} else {
+			empWrkHrs = 0;
+		}
+		int empWage = empWrkHrs * EMP_WAGE_PER_DAY;
+		System.out.println("empWage = " + empWage);
+	}
 }
 
 public class EmployeeWage {
 
-    public static void main(String[] args) {
-	System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
-	Employee.empCheck();
-    }
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+		int empCheck = Employee.empCheck();
+		Employee.empSalary(empCheck);
+	}
 
 }
